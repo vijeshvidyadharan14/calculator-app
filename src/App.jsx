aa680e89214d893ea201cdeca025ebdbf9bd147f
Vijesh <luckyvijesh@gmail.com>
2024-10-30 00:37:20 +0530

    calculator-app v01

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import CalBody from './CalBody'
+import CalBody from './components/CalBody'
 
 function App() {
   const [count, setCount] = useState(0)
